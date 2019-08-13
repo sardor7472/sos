f0851c0a92289d2743443df4fcb075a71d5bfdeb
shohrux <seltashoh@gmail.com>
2019-08-13 17:39:03 +0500

    home fixed bugs

--- app/js/main.js
+++ app/js/main.js
@@ -1,35 +1,51 @@
+new WOW().init();
 $('document').ready(function () {
-    $('.main-slider').owlCarousel({
-        loop: true,
+    var mainSlider = $('.main-slider')
+    mainSlider.owlCarousel({
+        loop: false,
         items: 1,
         dots: true,
-       // navText: ['1/', '3'],
-        nav: true,
+        nav: false,
+        onInitialized: function f() {
+            var t = this;
+                setTimeout(function () {
+                var  currSlide = t._current + 1,
+                    length = t._items.length;
+                $('.main-slider-count').html('<span style="margin-right:5px; color: #ffb500; font-size: 60px">' + currSlide + '</span>' + '/' + '<span style="font-size: 36px; margin-left: 5px;">' + 3 + '</span>');
+
+            },10)
+        }
     });
 
-        try {
-            $('.timepicker').wickedpicker({
-                now: '09:00',
-                twentyFour: true,
-                title: 'Выберите удобное время',
-                minutesInterval: 30,
-            });
-        }catch (e) {
-            console.log(e);
-        }
+    mainSlider.on('initialize.owl.carousel initialized.owl.carousel changed.owl.carousel resized.owl.carousel', function (e) {
+        owl_carousel_page_numbers(e);
+    });
+
+
+    function owl_carousel_page_numbers(e) {
+        var items_per_page = e.page.size;
+        var display_text = (e.item.index + 1);
+        $('.main-slider-count').html('<span style="margin-right:5px; color: #ffb500; font-size: 60px">' + display_text + '</span>' + '/' + '<span style="font-size: 36px; margin-left: 5px;">' + e.item.count + '</span>');
+
+    }
+
+
+    try {
+        $('.timepicker').wickedpicker({
+            now: '09:00',
+            twentyFour: true,
+            title: 'Выберите удобное время',
+            minutesInterval: 30,
+        });
+    } catch (e) {
+        console.log(e);
+    }
+
 
     $('select').niceSelect();
-    var i = 1;
     $('.header-forma-title').click(function () {
-        if(i % 2 === 1){
-            $('.header-forma').addClass('open-form'); i ++;
-        }
-        else if(i % 2 === 0){
-            $('.header-forma').removeClass('open-form'); i ++;
-        }
+        $('.header-forma').slideToggle(400)
     })
-
-
 
 
     // rating
@@ -39,18 +55,19 @@ $('document').ready(function () {
         var starRate = '<li class="active"><svg width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 33.73 32.23"><polygon class="cls-1" points="16.86 1.5 21.61 11.12 32.23 12.66 24.55 20.15 26.36 30.73 16.86 25.73 7.37 30.73 9.18 20.15 1.5 12.66 12.12 11.12 16.86 1.5"/></svg></li>'
         var list = '';
 
-        for(var i = 1; i < 11; i++) {
-            if( i > rate) {
+        for (var i = 1; i < 11; i++) {
+            if (i > rate) {
                 list += star;
             } else {
                 list += starRate;
-                console.log(i)
 
             }
-        };
+        }
+        ;
         $('.course-rating').html(list)
     }
-    rateRender()
 
+    rateRender();
+    // rating
 
 })
